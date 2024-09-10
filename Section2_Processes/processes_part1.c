daf082da069a3adc54377426bfee96fd6eb0c50f
Mohammed1306 <mohammed.kallel.2004@gmail.com>
2024-09-10 14:24:26 -0400

    2.2 done

--- Section2_Processes/processes_part1.c
+++ Section2_Processes/processes_part1.c
@@ -11,7 +11,61 @@
 
 void question1()
 {
-    // TODO
-    
+    pid_t lvl11, lvl12, lvl13, lvl21, lvl22, lvl23, lvl24, lvl25, lvl26;
+
+
+    lvl11 = fork();
+    if(lvl11 == 0){
+
+        lvl21 = fork();
+        if(lvl21 == 0)
+            _exit(0);
+
+        lvl22= fork();
+        if(lvl22 == 0)
+            _exit(0);
+
+        wait(NULL);
+        wait(NULL);
+
+    }
+
+
+    lvl12 = fork();
+    if(lvl12 == 0){
+
+        lvl23 = fork();
+        if(lvl23 == 0)
+            _exit(0);
+
+        wait(NULL);
+
+    }
+
+    lvl13 = fork();
+    if(lvl13 == 0){
+
+        lvl24 = fork();
+        if(lvl24 == 0)
+            _exit(0);
+
+        lvl25 = fork();
+        if(lvl25 == 0)
+            _exit(0);
+
+        lvl26 = fork();
+        if(lvl26 == 0)
+            _exit(0);
+
+        wait(NULL);
+        wait(NULL);
+        wait(NULL);
+
+    }
+
+
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
 }
 
