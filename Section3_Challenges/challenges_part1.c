56e5b5ee6899e16f923d4f92d0e38b9553ff4b0f
Mohammed1306 <mohammed.kallel.2004@gmail.com>
2024-09-12 12:31:42 -0400

    5.1 half done

--- Section3_Challenges/challenges_part1.c
+++ Section3_Challenges/challenges_part1.c
@@ -7,9 +7,53 @@
 */
 #include "challenges_part1.h"
 
+
+int ExpolrerRepertoire(char *chemin){
+    DIR *repertoire = opendir(chemin);
+
+    int cntFichiers = 0;
+
+    dirent *entre;
+
+    while((entre = readdir(repertoire)) != NULL){
+        if (strcmp(entre->d_name, ".") == 0 || strcmp(entre->d_name, "..") == 0) {
+            continue;
+        }
+
+        
+        char cheminComplet[MAX_PATH_LENGTH];
+        snprintf(cheminComplet, sizeof(cheminComplet), "%s/%s", chemin, entre->d_name);
+
+        if (entre->d_type == DT_REG) {
+            cntFichiers++;
+        } else {
+            if(fork() == 0){
+                int count = ExpolrerRepertoire(cheminComplet);
+                _exit(count);
+            }
+        }
+
+    }
+
+    closedir(repertoire);
+
+    int status;
+
+    while (wait(&status) > 0) {
+        if (WIFEXITED(status)) {
+            cntFichiers += WEXITSTATUS(status);
+        }
+    }
+
+    return cntFichiers;
+}
+
 int main(int argc, char*argv[])
 {
-    // TODO
+    int somme = ExpolrerRepertoire("root");
+
+    printf("Nombre total de fichiers texte: %d\n", somme);
+
     return 0;
 }
 
