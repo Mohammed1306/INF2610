402e0497d2189b2aae01d15144ac3ebd80e1e3d5
johas <johas@L4818-05>
2024-11-26 17:34:12 -0500

    partie 2 a moitié done

--- TP3_Groupe3/TP3_Groupe3/Section 2/GestionMemoire.c
+++ TP3_Groupe3/TP3_Groupe3/Section 2/GestionMemoire.c
@@ -1,27 +1,47 @@
 #include<stdbool.h>
 #include "./libs/lib.h"
 
+const unsigned int TAILLE_PAGE = 1024;
+
 unsigned int calculerNumeroDePage(unsigned long adresse) {
-	// TODO
-    return 0;
+    return adresse / TAILLE_PAGE;
 }
 
 unsigned long calculerDeplacementDansLaPage(unsigned long adresse) {
-	// TODO
-     return 0;
+    return adresse % TAILLE_PAGE;
 }
 
 unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage) {
-	// TODO
-     return 0;
+    return numeroDePage * TAILLE_PAGE + deplacementDansLaPage;
 }
 
 void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+	unsigned int numeroPageVirtuelle = calculerNumeroDePage(req->adresseVirtuelle);
+    for (int i = 0; i < TAILLE_TLB; i++) {
+        if (mem->tlb->entreeValide[i] && mem->tlb->numeroPage[i] == numeroPageVirtuelle) {
+            unsigned int numeroCadre = mem->tlb->numeroCadre[i];
+            unsigned long deplacement = calculerDeplacementDansLaPage(req->adresseVirtuelle);
+            req->adressePhysique = calculerAdresseComplete(numeroCadre, deplacement);
+            mem->tlb->dernierAcces[i] = req->date;
+            req->estDansTLB = 1;
+            return;
+        }
+    }
+    req->adressePhysique = 0;  
+    req->estDansTLB = 0;    
 }
 
 void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+	unsigned int numeroPageVirtuelle = calculerNumeroDePage(req->adresseVirtuelle);
+	unsigned int numeroCadre = mem->tp->numeroCadre[numeroPageVirtuelle];
+	if (mem->tp->entreeValide[numeroPageVirtuelle]) {
+		unsigned long deplacement = calculerDeplacementDansLaPage(req->adresseVirtuelle);
+		req->adressePhysique = calculerAdresseComplete(numeroCadre, deplacement);
+		req->estDansTablePages = 1;
+	} else {
+		req->adressePhysique = 0;  
+    	req->estDansTablePages = 0; 
+	}
 }
 
 void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
